6c383b1dcc9a481c4f73d07a7d145f81721e4b4d
huita <ramzisahra@gmail.com>
2022-01-25 19:30:39 +0100

    gmao m c

---
+++ app/Http/Controllers/HeursSuppController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use DB;
+use Brian2694\Toastr\Facades\Toastr;
+use Session;
+use Auth;
+use App\Models\;
+
+class HeursSuppController extends Controller
+{
+    //
+}
---
+++ database/migrations/2022_01_25_181533_create_magasins_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMagasinsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('magasins', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('magasins');
+    }
+}
